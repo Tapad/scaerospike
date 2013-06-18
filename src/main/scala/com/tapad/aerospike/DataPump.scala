38246ceffaa9d45d0a5734f36466bdcd2c9ec1db
Dag Liodden <dag@tapad.com>
2013-06-18 02:25:11 -0400

    Try async writes again, but with own executor to avoid deadlocks

--- src/main/scala/com/tapad/aerospike/DataPump.scala
+++ src/main/scala/com/tapad/aerospike/DataPump.scala
@@ -3,9 +3,11 @@ package com.tapad.aerospike
 import com.aerospike.client.async.{AsyncClientPolicy, MaxCommandAction, AsyncClient}
 import java.util.concurrent.atomic.AtomicInteger
 import com.aerospike.client.policy.{ClientPolicy, WritePolicy, ScanPolicy}
-import com.aerospike.client.{Bin, Record, Key, ScanCallback}
+import com.aerospike.client._
 import java.util
 import scala.collection.JavaConverters._
+import com.aerospike.client.listener.WriteListener
+import java.util.concurrent.Executors
 
 object DataPump {
   def main(args: Array[String]) {
@@ -14,25 +16,44 @@ object DataPump {
     val destAddr = args(1)
     val namespace = args(2)
 
-    val clientPolicy = new AsyncClientPolicy
-    clientPolicy.maxThreads = 256
-    clientPolicy.asyncMaxCommandAction = MaxCommandAction.BLOCK
-    clientPolicy.asyncSelectorThreads
+    val sourceClientPolicy = new AsyncClientPolicy
 
-    val source = new AsyncClient(clientPolicy, sourceAddr, 3000)
-    val destination = new AsyncClient(clientPolicy, destAddr ,3000)
+    val destClientPolicy = new AsyncClientPolicy
+    destClientPolicy.asyncMaxCommandAction = MaxCommandAction.BLOCK
+    destClientPolicy.asyncMaxCommands = 1000
+    destClientPolicy.asyncTaskThreadPool = Executors.newCachedThreadPool()
+
+    val source = new AsyncClient(sourceClientPolicy, sourceAddr, 3000)
+    val destination = new AsyncClient(destClientPolicy, destAddr ,3000)
 
     println("Copying all data from namespace %s from cluster at %s to %s...".format(namespace, sourceAddr, destAddr))
 
     val recordsMoved = new AtomicInteger()
 
     val scanPolicy = new ScanPolicy()
-    scanPolicy.threadsPerNode = 8
+    scanPolicy.threadsPerNode = 4
 
     val writePolicy = new WritePolicy()
 
     var startTime = System.currentTimeMillis()
     val batchSize = 100000
+
+    val writeListener = new WriteListener {
+      def onFailure(exception: AerospikeException) {
+        println("Error " + exception.toString)
+      }
+
+      def onSuccess(key: Key) {
+        val count = recordsMoved.incrementAndGet()
+        if (count % batchSize == 0) {
+          val elapsed = System.currentTimeMillis() - startTime
+          startTime = System.currentTimeMillis()
+          println("Processed %(,d bins / records, %d ms, %.2f records / sec".format(
+            count, elapsed, batchSize.toFloat / elapsed * 1000)
+          )
+        }
+      }
+    }
     source.scanAll(scanPolicy, namespace, "", new ScanCallback {
       def scanCallback(key: Key, record: Record) {
         val bins = new util.ArrayList[Bin]()
@@ -41,15 +62,7 @@ object DataPump {
           val e = i.next()
           bins.add(new Bin(e.getKey, e.getValue))
         }
-        destination.put(writePolicy, key, bins.asScala: _*)
-        val count = recordsMoved.incrementAndGet()
-        if (count % batchSize == 0) {
-          val elapsed = System.currentTimeMillis() - startTime
-          startTime = System.currentTimeMillis()
-          println("Processed %(,d records, %d ms, %.2f records / sec".format(
-            count, elapsed, batchSize.toFloat / elapsed * 1000)
-          )
-        }
+        destination.put(writePolicy, writeListener, key, bins.asScala: _*)
       }
     })
     println("Done, a total of %d records moved...".format(recordsMoved.get()))
