2b58c3d44958d94cf07f47f0b4b1c5834f22b1cf
Dag Liodden <dag@tapad.com>
2013-06-18 01:51:18 -0400

    Async writes

--- src/main/scala/com/tapad/aerospike/DataPump.scala
+++ src/main/scala/com/tapad/aerospike/DataPump.scala
@@ -3,9 +3,10 @@ package com.tapad.aerospike
 import com.aerospike.client.async.{AsyncClientPolicy, MaxCommandAction, AsyncClient}
 import java.util.concurrent.atomic.AtomicInteger
 import com.aerospike.client.policy.{ClientPolicy, WritePolicy, ScanPolicy}
-import com.aerospike.client.{Bin, Record, Key, ScanCallback}
+import com.aerospike.client._
 import java.util
 import scala.collection.JavaConverters._
+import com.aerospike.client.listener.WriteListener
 
 object DataPump {
   def main(args: Array[String]) {
@@ -32,6 +33,23 @@ object DataPump {
 
     var startTime = System.currentTimeMillis()
     val batchSize = 100000
+
+    val writeListener = new WriteListener {
+      def onFailure(exception: AerospikeException) {
+        println("Error " + exception.toString)
+      }
+
+      def onSuccess(key: Key) {
+        val count = recordsMoved.incrementAndGet()
+        if (count % batchSize == 0) {
+          val elapsed = System.currentTimeMillis() - startTime
+          startTime = System.currentTimeMillis()
+          println("Processed %(,d bins / records, %d ms, %.2f records / sec".format(
+            count, elapsed, batchSize.toFloat / elapsed * 1000)
+          )
+        }
+      }
+    }
     source.scanAll(scanPolicy, namespace, "", new ScanCallback {
       def scanCallback(key: Key, record: Record) {
         val bins = new util.ArrayList[Bin]()
@@ -40,15 +58,7 @@ object DataPump {
           val e = i.next()
           bins.add(new Bin(e.getKey, e.getValue))
         }
-        destination.put(writePolicy, key, bins.asScala: _*)
-        val count = recordsMoved.incrementAndGet()
-        if (count % batchSize == 0) {
-          val elapsed = System.currentTimeMillis() - startTime
-          startTime = System.currentTimeMillis()
-          println("Processed %(,d records, %d ms, %.2f records / sec".format(
-            count, elapsed, batchSize.toFloat / elapsed * 1000)
-          )
-        }
+        destination.put(writePolicy, writeListener, key, bins.asScala: _*)
       }
     })
     println("Done, a total of %d records moved...".format(recordsMoved.get()))
